d919ee61951fe9bc4aefa85ad29cd9c76264dc3b
shrutikashish <shruti.kashish8@gmail.com>
2025-05-26 16:16:23 +0530

    Added new projec

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Header from './header'
+
+
+
+function App() {
+  
+  return (
+    <div>
+     <h1>Header</h1>
+      <Header/>
+     <h2>code Step by step</h2>
+    </div>
+     
+    
+    
+  )
+}
+
+export default App
